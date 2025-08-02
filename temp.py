9a6d7fe352972edc91deec279c650e2bfdb1ed58
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2025-08-03 10:25:18 +1200

    version 2

--- temp.py
+++ temp.py
@@ -1,22 +1,16 @@
 import numpy as np
 
 temperatures_C = np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])
-
-
 avg_temp = np.mean(temperatures_C)
-print(f"Average Temperature (°C): {avg_temp:.2f}")
-
 max_temp = np.max(temperatures_C)
-print(f"Highest Temperature (°C): {max_temp}")
-
 min_temp = np.min(temperatures_C)
-print(f"Lowest Temperature (°C): {min_temp}")
-
-temperatures_f = (temperatures_c * 9/5) + 32
-print("\nTemperatures in Fahrenheit:")
-print(temperatures_f)
-
-days = [0]
-indices_above_20 = np.where(temperatures_c > 20)[days]
-print("\nIndices of days with temperature > 20°C:")
-print(indices_above_20)
+temperatures_f = (temperatures_C * 9/5) + 32
+indices_above_20 = np.where(temperatures_C > 20)[0]
+if __name__ == "__main__":
+    print(f"Average Temperature (°C): {avg_temp:.2f}")
+    print(f"Highest Temperature (°C): {max_temp}")
+    print(f"Lowest Temperature (°C): {min_temp}")
+    print("\nTemperatures in Fahrenheit:")
+    print(temperatures_f)
+    print("\nIndices of days with temperature > 20°C:")
+    print(indices_above_20)
