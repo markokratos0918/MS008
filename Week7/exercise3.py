d8f9964367512f27145069d5ac9a88ae2f37dae9
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2025-09-14 09:24:15 +1200

    Update exercise3.py

--- Week7/exercise3.py
+++ Week7/exercise3.py
@@ -24,3 +24,6 @@ class ShapeFactory:
 factory = ShapeFactory()
 shape = factory.create_shape("triangle")   
 print(shape.draw())  
+
+#Ans. 
+# The code becomes tightly coupled to specific classes. If you need to change the implementation or constructor of a class, you must update every place where it is instantiated.Using a Factory centralizes object creation, making your code more maintainable, flexible, and easier to test.
