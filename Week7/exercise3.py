e822ea84da41047978cedea862c59bf09d227f76
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2025-09-14 09:24:36 +1200

    Update exercise3.py

--- Week7/exercise3.py
+++ Week7/exercise3.py
@@ -26,4 +26,5 @@ shape = factory.create_shape("triangle")
 print(shape.draw())  
 
 #Ans. 
-# The code becomes tightly coupled to specific classes. If you need to change the implementation or constructor of a class, you must update every place where it is instantiated.Using a Factory centralizes object creation, making your code more maintainable, flexible, and easier to test.
+# The code becomes tightly coupled to specific classes. If you need to change the implementation or constructor of a class, you must update every place where it is instantiated.
+# Using a Factory centralizes object creation, making your code more maintainable, flexible, and easier to test.
