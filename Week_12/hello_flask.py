9f4d5a9848e06b1276f92ca5a71ade8e3f6a12df
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2025-10-18 11:43:06 +1300

    Activity 1.1

--- Week_12/hello_flask.py
+++ Week_12/hello_flask.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_flask():
     return ("<h1>'Hello, Flask Activity 1!'</h1>")
+
+@app.route("/marko")
+def marko():
+    return ("<h2>'Marko is Flask!'</<h2>")
